2c5c50ecf3a18d186f6175a48cb8d5ebccaf9b53
steph-crown <emmanuelstephen024@gmail.com>
2026-01-24 22:16:10 +0100

    Implement algorithm 1877

--- algorithms/leetcode/rust/src/algos.rs
+++ algorithms/leetcode/rust/src/algos.rs
@@ -1,6 +1,7 @@
 pub mod p121_best_time_to_buy_and_sell_stocks;
 pub mod p1390_four_divisors;
 pub mod p1411_no_ways_to_paint_grid;
+pub mod p1877_minimize_maximum_pair_sum_in_array;
 pub mod p1975_max_matrix_sum;
 pub mod p1_two_sum;
 pub mod p217_contains_duplicate;
