fb4895d74ab4d99b1b54c8787c59553a2a3cc07e
steph-crown <emmanuelstephen024@gmail.com>
2026-03-27 23:30:59 +0100

    Implement remove function in DLL

--- data-structures/python/doubly_linked_list.py
+++ data-structures/python/doubly_linked_list.py
@@ -30,7 +30,9 @@ class DoublyLinkedList:
 
   def display(self):
     curr = self.head
-    if curr is not None:
+    if curr is None:
+      return ""
+    else:
       res = f"{curr.val}"
       curr = curr.next
     while curr is not None:
@@ -69,6 +71,45 @@ class DoublyLinkedList:
       i += 1
     self.size += 1
 
+  def pop(self):
+    if self.tail:
+      val = self.tail.val
+      prev = self.tail.prev
+      if prev is None:
+        self.head = None
+        self.tail = None
+      else:
+        prev.next = None
+        self.tail = prev
+      self.size -= 1
+      return val
+
+  def pop_left(self):
+    if self.head:
+      val = self.head.val
+      next_node = self.head.next
+      if next_node is None:
+        self.head = None
+        self.tail = None
+      else:
+        next_node.prev = None
+        self.head = next_node
+      self.size -= 1
+      return val
+
+  def remove(self, val):
+    curr = self.head
+    while curr is not None:
+      if curr.val == val:
+        prev = curr.prev
+        next_node = curr.next
+
+        if prev is not None:
+          prev.next = next_node
+        if next_node is not None:
+          next_node.prev = prev
+      curr = curr.next
+
   def __len__(self):
     return self.size
 
@@ -83,4 +124,11 @@ list.append(2)
 list.append(4)
 list.prepend(49)
 list.insert(23, 2)
+print(list)
+
+print(list.pop())
+print(list)
+
+# print(list.pop_left())
+list.remove(23)
 print(list)
