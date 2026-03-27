29cbd7f77edc431f8494f3c84ed903de19cdaa31
steph-crown <emmanuelstephen024@gmail.com>
2026-03-27 21:44:18 +0100

    .

---
+++ data-structures/python/doubly_linked_list.py
@@ -0,0 +1,50 @@
+class Node:
+  def __init__(self, val, prev = None, next = None) -> None:
+    self.val = val
+    self.prev = prev
+    self.next = next
+
+  def __str__(self) -> str:
+    return str(self.val)
+
+class DoublyLinkedList:
+  def __init__(self) -> None:
+    self.head = None
+    self.tail = None
+    self.size = 0
+
+  @property
+  def is_empty(self):
+    return self.head is not None
+
+  def append(self, val):
+    node = Node(val)
+    if self.tail is None:
+      self.head = node
+      self.tail = node
+    else:
+      self.tail.next = node
+      node.prev = self.tail
+      self.tail = node
+    self.size += 1
+
+  def display(self):
+    curr = self.head
+    if curr is not None:
+      res = f"{curr.val}"
+      curr = curr.next
+    while curr is not None:
+      res = f"{res} <-> {curr.val}"
+      curr = curr.next
+    return res
+
+  def __str__(self) -> str:
+    return f"{self.display()} \t size: {self.size} \t tail: {self.tail}"
+
+
+list = DoublyLinkedList()
+list.append(3)
+list.append(9)
+list.append(2)
+list.append(4)
+print(list)
